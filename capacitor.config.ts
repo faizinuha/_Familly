29139bb59843731a902c74bbfaa5d64a62adb854
TZ <rozakadm@gmail.com>
2025-06-22 19:42:31 +0700

    chore: update version numbers in build.gradle and package.json; modify appId and appName in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'com.good-family.app',
+  appId: 'com.Vvi_studio.app',
+  appName: 'Family',
   webDir: 'dist'
 };
 
